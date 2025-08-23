60a1ec0846db038b658a2b0a44c369eae50446a4
SHREY KHATER <shrey@Shreys-MacBook-Pro.local>
2025-08-23 14:29:26 -0700

    added navbar

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function Navbar() {
+    return (
+        <nav className="navBar">
+            <ul className="navLinks">
+                <li>Home</li>
+                <li>Books</li>
+                <li>Movies</li>
+                <li>Music</li>
+                <li>Projects</li>
+                <li>Thoughts</li>
+            </ul>
+        </nav>
+    );
+}
+export default Navbar;
