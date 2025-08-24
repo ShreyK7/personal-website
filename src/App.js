9aa09294a2df79737a14441964276d2d59aee809
SHREY KHATER <shrey@Shreys-MBP.lan>
2025-08-24 10:57:48 -0700

    added styling and animation effect for home page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         Hi, I'm Shrey Khater
       </h2>
       <p className = "aboutText">
-        I'm a senior at NYU (graduating Dec 2025) double majoring in Computer Science and NYU Stern's Business, Technology, & Entrepreneurship program (sorry if that's a mouthful). I've worked at a web3 startup called DoraHacks doing quantum computing software research and software development as well as interned at Capital One working in IOS dev. I love seeing new places (30 countries and counting), reading, watching movies, anything to do with music, and creating lifelong experiences with the people closest to me :)
+        I'm a senior at NYU (graduating Dec 2025) double majoring in Computer Science and NYU Stern's Business, Technology, & Entrepreneurship program (sorry if that's a mouthful). I've worked at a web3 startup called DoraHacks doing quantum computing software research and software development as well as interned at Capital One working in IOS dev. I love seeing new places (30 countries and counting!), music that defies genres, reading, watching movies, and creating lifelong experiences with my friends and family :)
       </p>
     </div>
   );
